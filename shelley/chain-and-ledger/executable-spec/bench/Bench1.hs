ba6481a6e04e9fa8ffd91930cf20cc5f4aa02488
TimSheard <tim.sheard@relational.ai>
2020-06-03 16:06:27 -0700

    Work on Bench1.hs

--- shelley/chain-and-ledger/executable-spec/bench/Bench1.hs
+++ shelley/chain-and-ledger/executable-spec/bench/Bench1.hs
@@ -8,43 +8,54 @@ import Test.Tasty.HUnit (Assertion, (@?=))
 import Test.Shelley.Spec.Ledger.Utils (runShelleyBase)
 import Control.State.Transition.Extended (PredicateFailure, TRC (..), applySTS)
 import Control.State.Transition.Trace ((.-), (.->), checkTrace)
-import Test.Shelley.Spec.Ledger.ConcreteCryptoTypes (
-  LedgerEnv, UTxOState, Tx, DPState, LEDGER)
-import Test.Shelley.Spec.Ledger.Examples (aliceAddr, txEx2A, ppsEx1)
+import Test.Shelley.Spec.Ledger.ConcreteCryptoTypes (LedgerEnv, UTxOState, Tx, TxOut, DPState, LEDGER)
+import Test.Shelley.Spec.Ledger.Examples ( aliceAddr, txEx2A, ppsEx1)
+
+
 import Shelley.Spec.Ledger.PParams (emptyPPPUpdates)
 import Shelley.Spec.Ledger.LedgerState (genesisCoins, emptyDPState, pattern UTxOState)
 import Shelley.Spec.Ledger.TxData (pattern TxOut)
-import Shelley.Spec.Ledger.Tx (pattern Tx)
+-- import Shelley.Spec.Ledger.Tx (pattern Tx)
 import Shelley.Spec.Ledger.Coin (Coin (..))
 import Shelley.Spec.Ledger.Slot (SlotNo (..))
 import Shelley.Spec.Ledger.STS.Ledger (pattern LedgerEnv)
 
-utxoState :: UTxOState
-utxoState =
+
+coins :: Integer -> [TxOut]
+coins n = fmap (\x -> TxOut aliceAddr (Coin $ 100+x)) [0..n]
+
+utxoState :: Integer -> UTxOState
+utxoState _n =
   UTxOState
-    ( genesisCoins $ []
-        --fmap
-        --  (\x -> TxOut aliceAddr (Coin $ 100+x))
-        --  [0..n]
-    )
+    ( genesisCoins (coins _n) )
     (Coin 0)
     (Coin 0)
     emptyPPPUpdates
 
+
 ledgerEnv :: LedgerEnv
 ledgerEnv = LedgerEnv (SlotNo 0) 0 ppsEx1 0
 
-testLEDGER ::
-  (UTxOState, DPState) ->
-  Tx ->
-  LedgerEnv ->
-  Either [[PredicateFailure LEDGER]] (UTxOState, DPState) ->
-  Assertion
+
+testLEDGER :: (UTxOState, DPState)
+                    -> Tx
+                    -> LedgerEnv
+                    -> Either
+                         [[PredicateFailure LEDGER]]
+                         (UTxOState, DPState)
+                    -> Assertion
+
+
 testLEDGER initSt tx env (Right expectedSt) = do
   checkTrace @LEDGER runShelleyBase env $ pure initSt .- tx .-> expectedSt
 testLEDGER initSt tx env predicateFailure@(Left _) = do
   let st = runShelleyBase $ applySTS @LEDGER (TRC (env, initSt, tx))
   st @?= predicateFailure
+
+
+ledger :: Integer -> Assertion
+ledger _n = testLEDGER (utxoState _n, emptyDPState) txEx2A ledgerEnv (Left [])
+
 
 -- The function we're benchmarking.
 fib:: Int -> Int
@@ -55,8 +66,6 @@ fib m | m < 0     = error "negative!"
     go 1 = 1
     go n = go (n-1) + go (n-2)
 
-ledger :: Integer -> Assertion
-ledger _n = testLEDGER (utxoState, emptyDPState) txEx2A ledgerEnv (Left [])
 
 -- Our benchmark harness.
 main :: IO ()
@@ -65,9 +74,8 @@ main = defaultMain [
                , bench "5"  $ whnf fib 5
                , bench "9"  $ whnf fib 9
                , bench "11" $ whnf fib 11
-               ],
-  bgroup "ledger" [ bench "4"  $ whnf ledger 4
-                  ]
+               ]
+  , bgroup "ledger" [ bench "4"  $ whnf ledger 4]
   ]
 
 
