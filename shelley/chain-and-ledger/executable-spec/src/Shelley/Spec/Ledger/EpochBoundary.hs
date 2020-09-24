IntersectMBO/cardano-ledger

Parametrise the Shelley ledger over Value.
This PR demonstrates a way to parametrise the ledger over value. It
paves the way for overriding this type in subsequent eras, without
making any semantic changes to the Shelley ledger.

Functions in the ledger are now classfied into three groups:
- Those that remain universally quantified over eras.
- Those that have specific requirements on certain era-parametrised
types to work (e.g. that they can be serialised), and
- Those that are specific to Shelley.

There are two examples of the latter: `consumed` and `produced`.

The STS rules are likewise split in two:
- Those that remain universally quantified over eras.
- Those that are specific to Shelley.

The intention is that many will transition to the third (missing)
category, where the actual rules specify their exact requirements on the
era (see #1804 for an example of how to do this). This was not done
because I wanted to get this ready for the meeting and I didn't have
time.

This PR makes some very deliberate design choices:
- The 'Value' type family and any constraints thereon are _not_ lifted
into the 'Era' class. Such would imply universality over eras, which is
too strong a constraint. They would prevent us, for example, from adding to
the `Val` class in future eras, which seems quite plausible.
Furthermore, having more explicit constraints at the use site helps our
understanding of the code; it is much more obvious _when_ we are relying
on certain things, which can be a tool to spot bugs. Note that these
constraints do not propogate wildly, because in general they are
dispatched through the instantiation of the type family to a specific
type in a given era.
- It deliberately removes some instances (`Eq` for example) where it
seems there is no strong reason to have it. The presence of an `Eq`
instance everywhere has bitten us in the past, where for example we have
ended up calling `(==)` on a large part of state in the consensus
integration, which is an expensive

shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/EpochBoundary.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
 
 -- |
 -- Module      : EpochBoundary
@@ -27,6 +30,7 @@ module Shelley.Spec.Ledger.EpochBoundary
 where
 
 import Cardano.Binary (FromCBOR (..), ToCBOR (..), encodeListLen)
+import qualified Cardano.Ledger.Core as Core
 import Cardano.Ledger.Era
 import qualified Cardano.Ledger.Val as Val
 import Cardano.Prelude (NFData, NoUnexpectedThunks (..))
@@ -42,7 +46,6 @@ import Shelley.Spec.Ledger.Coin
   ( Coin (..),
     coinToRational,
     rationalToCoinViaFloor,
-    word64ToCoin,
   )
 import Shelley.Spec.Ledger.Credential (Credential, Ptr, StakeReference (..))
 import Shelley.Spec.Ledger.DeserializeShort (deserialiseAddrStakeRef)
@@ -78,7 +81,11 @@ newtype Stake era = Stake
 
 -- | Sum up all the Coin for each staking Credential
 aggregateUtxoCoinByCredential ::
-  Era era =>
+  forall era.
+  ( Era era,
+    Val.Val (Core.Value era),
+    Core.Compactible (Core.Value era)
+  ) =>
   Map Ptr (Credential 'Staking era) ->
   UTxO era ->
   Map (Credential 'Staking era) Coin ->
@@ -86,12 +93,14 @@ aggregateUtxoCoinByCredential ::
 aggregateUtxoCoinByCredential ptrs (UTxO u) initial =
   Map.foldr accum initial u
   where
-    accum (TxOutCompact addr c) ans = case deserialiseAddrStakeRef addr of
-      Just (StakeRefPtr p) -> case Map.lookup p ptrs of
-        Just cred -> Map.insertWith (<>) cred (word64ToCoin c) ans
-        Nothing -> ans
-      Just (StakeRefBase hk) -> Map.insertWith (<>) hk (word64ToCoin c) ans
-      _other -> ans
+    accum (TxOutCompact addr c) ans =
+      let c' = Val.coin . Core.fromCompact @(Core.Value era) $ c
+       in case deserialiseAddrStakeRef addr of
+            Just (StakeRefPtr p) -> case Map.lookup p ptrs of
+              Just cred -> Map.insertWith (<>) cred c' ans
+              Nothing -> ans
+            Just (StakeRefBase hk) -> Map.insertWith (<>) hk c' ans
+            _other -> ans
 
 -- | Get stake of one pool
 poolStake ::

obsidiansystems/cardano-ledger-specs

shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/EpochBoundary.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
 
 -- |
 -- Module      : EpochBoundary
@@ -27,6 +30,7 @@ module Shelley.Spec.Ledger.EpochBoundary
 where
 
 import Cardano.Binary (FromCBOR (..), ToCBOR (..), encodeListLen)
+import qualified Cardano.Ledger.Core as Core
 import Cardano.Ledger.Era
 import Cardano.Ledger.Val ((<+>), (<×>))
 import Cardano.Prelude (NFData, NoUnexpectedThunks (..))
@@ -41,7 +45,6 @@ import Shelley.Spec.Ledger.Coin
   ( Coin (..),
     coinToRational,
     rationalToCoinViaFloor,
-    word64ToCoin,
   )
 import Shelley.Spec.Ledger.Credential (Credential, Ptr, StakeReference (..))
 import Shelley.Spec.Ledger.DeserializeShort (deserialiseAddrStakeRef)
@@ -77,7 +80,11 @@ newtype Stake era = Stake
 
 -- | Sum up all the Coin for each staking Credential
 aggregateUtxoCoinByCredential ::
-  Era era =>
+  forall era.
+  ( Era era,
+    Core.ValType era,
+    Val.Val (Core.Value era)
+  ) =>
   Map Ptr (Credential 'Staking era) ->
   UTxO era ->
   Map (Credential 'Staking era) Coin ->
@@ -85,12 +92,14 @@ aggregateUtxoCoinByCredential ::
 aggregateUtxoCoinByCredential ptrs (UTxO u) initial =
   Map.foldr accum initial u
   where
-    accum (TxOutCompact addr c) ans = case deserialiseAddrStakeRef addr of
-      Just (StakeRefPtr p) -> case Map.lookup p ptrs of
-        Just cred -> Map.insertWith (<>) cred (word64ToCoin c) ans
-        Nothing -> ans
-      Just (StakeRefBase hk) -> Map.insertWith (<>) hk (word64ToCoin c) ans
-      _other -> ans
+    accum (TxOutCompact addr c) ans =
+      let c' = Val.coin . Core.fromCompact @(Core.Value era) $ c
+       in case deserialiseAddrStakeRef addr of
+            Just (StakeRefPtr p) -> case Map.lookup p ptrs of
+              Just cred -> Map.insertWith (<>) cred c' ans
+              Nothing -> ans
+            Just (StakeRefBase hk) -> Map.insertWith (<>) hk c' ans
+            _other -> ans
 
 -- | Get stake of one pool
 poolStake ::